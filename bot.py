44b8cd5c3f8acd7c55dd42608f10707862899167
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-28 19:45:38 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,31 @@
-# https://sechat.readthedocs.io/en/latest/ for SEChat documentation
+import sechat
+from sechat.events import Events
+import sys
+import os
+import re
+import html
+
+USERNAME = sys.argv[1]
+ROOM = sys.argv[2]
+EMAIL = os.environ["email"]
+PASSWORD = os.environ["password"]
+PHRASE = f"🐟 <i>{USERNAME}'s line quivers.</i>"
+
+bot = sechat.Bot()
+bot.login(EMAIL, PASSWORD)
+r = bot.joinRoom(ROOM)
+r.send("/fish")
+
+def msg(e):
+  if html.unescape(e.content) == PHRASE and e.user_id == 375672:
+    r.send("/fish")
+    r.send("I am automatically fishing! I am using [this Github repository](https://github.com/PlaceReporter99/auto-fish-action/) to do it. Note: this is an automatic message.")
+    r.send("/fish")
+
+r.on(Events.MESSAGE, msg)
+
+try:
+  while True:
+    pass
+finally:
+  sys.exit(0)
